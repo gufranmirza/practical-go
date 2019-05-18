a79377bc89f39a2783f5eb54c0924df8ac3e2f10
Mike J <gufranmirza1@gmail.com>
2019-05-19 00:33:47 +0530

    Fix package naming

---
+++ mathutils/mathutils.go
@@ -0,0 +1,24 @@
+
+package mathutils
+
+
+// Returns Addition of two numbers
+func Addition(a int, b int) int {
+        return a+b
+}
+
+// Returns Substraction of two numbers
+func Substraction(a int, b int) int {
+        return a-b
+}
+
+// Returns Multiplication of two numbers
+func Multiplication(a int, b int) int {
+        return a*b
+}
+
+// Returns Square of two numbers
+func Square(a int) int {
+        return a*a
+}
+
